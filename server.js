10c67c8cc7ec982b23304e231f5162f8d5f83ee3
Eric Rohner <ericrohner22@gmail.com>
2018-10-29 19:18:04 -0600

    technically pong

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ let gameState = {
   height: 80,
   width: 15,
   velocityY: 5,
-  color: "#ffffff"
+  color: "#ffffff",
+  score: 0
   },
   player2:{
     assigned: 0,
@@ -38,7 +39,8 @@ let gameState = {
     height: 80,
     width: 15,
     velocityY: 5,
-    color: "#ffffff"
+    color: "#ffffff",
+    score: 0
   },
   ball:{
     x: 375,
@@ -53,7 +55,7 @@ let gameState = {
 //execute this code for every connection made
 io.on('connection', (socket) => {
   //when the player sends a new player message
-  socket.on('new player', () => {
+  socket.on('new player', function() {
     //assign player1 if it's unclaimed
     if (!gameState.player1.assigned){
       sockets[socket.id] = {player: 1};
@@ -107,5 +109,69 @@ io.on('connection', (socket) => {
 });
 //attempt to send state at 60 fps
 setInterval(() => {
+  //collision y
+  if (
+    gameState.ball.y + gameState.ball.velocityY <= 0 ||
+    gameState.ball.y + gameState.ball.velocityY + gameState.ball.height >=
+    500
+  ) {
+    gameState.ball.velocityY = gameState.ball.velocityY * -1
+    gameState.ball.x += gameState.ball.velocityX
+    gameState.ball.y += gameState.ball.velocityY
+  } else {
+    gameState.ball.x += gameState.ball.velocityX
+    gameState.ball.y += gameState.ball.velocityY
+  }
+
+  // collision x
+  if (
+    gameState.ball.x + gameState.ball.velocityX <=
+    gameState.player1.x + gameState.player1.width &&
+    gameState.ball.y + gameState.ball.velocityY > gameState.player1.y &&
+    gameState.ball.y + gameState.ball.velocityY <=
+    gameState.player1.y + gameState.player1.height
+  ) {
+    // Fixed bug where "catching" the ball with the top or bottom of the paddle
+    // would result in it switching X direction every frame. Since few players have frame
+    // perfect timing this essentially gives the top and bottom of the paddle a 50/50 chance
+    // to lose the volley. Players don't like random chance hurting them in a skill game.
+    gameState.ball.velocityX = Math.abs(gameState.ball.velocityX)
+   
+  } else if (
+    gameState.ball.x + gameState.ball.width + gameState.ball.velocityX >=
+    gameState.player2.x &&
+    gameState.ball.y + gameState.ball.velocityY > gameState.player2.y &&
+    gameState.ball.y + gameState.ball.velocityY <=
+    gameState.player2.y + gameState.player2.height
+  ) {
+    // see bug fix above
+    gameState.ball.velocityX = (Math.abs(gameState.ball.velocityX)) * -1
+    
+  } else if (
+    gameState.ball.x + gameState.ball.velocityX <
+    gameState.player1.x - gameState.player1.width
+  ) {
+    gameState.player2.score += 1
+    
+    //if (this.p2Score >= this.props.pointsToWin) {
+      //this.winner = 2
+      //this.props._changeGameStart()
+    //}
+    gameState.ball = ({ ...gameState.ball, x: 375, y: 250 })
+  } else if (
+    gameState.ball.x + gameState.ball.velocityX >
+    gameState.player2.x + gameState.player2.width
+  ) {
+    gameState.player1.score += 1
+    //if (this.p1Score >= this.props.pointsToWin) {
+      //this.winner = 1
+      //this.props._changeGameStart()
+    //}
+    gameState.ball = ({ ...gameState.ball, x: 375, y: 250 })
+  } else {
+    gameState.ball.x += gameState.ball.velocityX
+    gameState.ball.y += gameState.ball.velocityY
+  }
+  //send updated gameState
   io.sockets.emit('state', gameState);
 }, 1000 / 60);
